6b29bbf81c009aef3fe1a040c7a7fa25444defed
Leemainsw <s2018s15@e-mirim.hs.kr>
2020-06-02 09:42:03 +0900

    setting str 추가 및 예제 추가

--- JS/Lock.js
+++ JS/Lock.js
@@ -1,43 +1,51 @@
 var KeyText = document.querySelector(".inputKey"); //사용자가 입력한 암호키
 var InputText = document.querySelector(".inputTEXT"); //사용자가 입력한 평문
 
+var board = new Array(4);
+    for (i = 0; i <=4; i++){
+        board[i] = new Array(5);
+    }
+
 function init(){
     //키 셋팅 및 board 셋팅
     SettingBoard();
-    console.log(SettingStr());
-
+    SettingStr(inputText.value);
+    //console.log(SettingStr());
 }
 
-function SettingStr(){
-    //평문 셋팅
-    var InputValueText = InputText.value; 
-    var InputValue = BlankDelete(InputValueText)  // 공백제거한 평문
-    // 1. 두 글자씩 자른다.
-    // 2. 두 글자가 똑같으면 하나는 X와 같은 채움문자로 셋팅한다.
-    // 3. 글자가 홀수 이면 마지막 문자는 X와 같은 채움문자로 셋팅한다.
-
-    var Value;
-
-    for(var i = 0; i < InputValueText.length; i+2){
-        var temp = InputValueText.substring(i, 2);
-        if(temp[0] == temp[1]){
-            temp[1] ='x';
-            Value += temp;
+function SettingStr(str){
+    for( let i = 0 ; i < str.length() ; i++ ) 
+    {
+        if(str.charAt(i)==' ') //공백제거
+        {
+            str = str.substring(0,i)+str.substring(i+1,str.length);
+            blankCheck+=10;
         }
-        else{
-            Value += temp;
+        else
+        {
+            blankCheck+=0;
+        }
+        if(str.charAt(i)=='z') //z를 q로 바꿔줘서 처리함.
+        {
+            str = str.substring(0,i)+'q'+str.substring(i+1,str.length);
+            zCheck+=1;
+        }
+        else 
+        {
+            zCheck+=0;
         }
     }
-    return Value;
+    return str;
 }
 
+//RealKey의 값을 
 function SettingBoard(){
     //값 가져오기
     var KeyValueText = KeyText.value;
 
     //공백 제거하기
     var KeyValue = BlankDelete(KeyValueText); // 공백제거한 키값
-
+2
     //여기는 키 셋팅
         // key 중복제거 및 alphabet 셋팅
         var key = String(overlap(KeyValue)); //중복제거한 키값
@@ -46,6 +54,12 @@ function SettingBoard(){
         // 키값에서 중복제거
         var RealKey = overlap(key);
         RealKey.splice(1,1);
+
+        for(let i=0; i<25; i++){
+            for(let j=0; j<5; j++){
+                console.log(board[i][j])
+            }
+        }
 }
 
 // blank 제거함수
---
+++ test.js
@@ -0,0 +1,92 @@
+console.log(encrypt("be careful for assassinator", "assassinator"));
+
+function encrypt(plain, key) {
+    //key = SettingBoard(key);
+    key = constructKeyMatrix(key);
+    plain = editPlain(plain);
+    result = encryptPlayFair(plain, key);
+    return result;
+}
+
+function decrypt(cipher, key) {
+  //currently not supported
+    return cipher;
+}
+
+// 키 setting
+function constructKeyMatrix(key) {
+    const alphabet = "abcdefghijklmnopqrstuvwxy";
+    key += alphabet;
+    for (let i = 0; i < key.length; i++) {
+    if (key.indexOf(key[i]) !== i) {
+        key = key.slice(0, i) + key.slice(i + 1); //abcdefghi => abcd + fghi
+        i--;
+    }
+
+    }
+    return key;
+}
+
+//중복제거 함수
+function overlap(overlapText){
+    var arr = [], dupArr = [];
+    for(var i=0; i<overlapText.length; i++) {
+        var t = overlapText[i].toLowerCase();
+        if(arr.indexOf(t)<0) arr.push(t);
+        else if(dupArr.indexOf(t)<0) dupArr.push(t);
+    }
+    return arr;
+}
+
+function SettingBoard(key){
+    key += "abcdefghijklmnopqrstuvwxy";
+    // 키값에서 중복제거
+    var RealKey = overlap(key);
+    RealKey.splice(1,1);
+
+    return RealKey;
+}
+
+
+// 평문편집
+function editPlain(plain) {
+    for (let i = 0; i < plain.length - 1; i += 2) {
+    if (plain[i] === plain[i + 1])
+        plain = plain.slice(0, i + 1) + 'x' + plain.slice(i + 1);
+    }
+
+    if (plain.length % 2 === 1) plain += 'x';
+
+    plain = plain.replace(/z/g, 'q'); 
+    return plain;
+}
+
+// 검사
+function encryptPlayFair(plaintext, key) {
+    var ciphertext = "";
+    for (let i = 0; i < plaintext.length - 1; i += 2) {
+        var i1, i2, j1, j2;
+        // index (1d) --> i = index / 5 , j = index % 5 (2d)
+        //5/2 = 2.5 | 0 = 2
+        i1 = key.indexOf(plaintext[i]) / 5 | 0; //integer
+        j1 = key.indexOf(plaintext[i]) % 5;
+
+        i2 = key.indexOf(plaintext[i + 1]) / 5 | 0; //integer
+        j2 = key.indexOf(plaintext[i + 1]) % 5;
+
+        //same row
+        if (i1 == i2)
+        // i1, (j1 + 1) % 5 and i2, (j2 + 1) % 5
+        ciphertext += key[i1 * 5 + (j1 + 1) % 5] + key[i2 * 5 + (j2 + 1) % 5];
+
+        //same column
+        else if (j1 == j2)
+        //(i1 + 1) % 5, j1 and (i2 + 1) % 5, j2
+        ciphertext += key[((i1 + 1) % 5)] + key[((i2 + 1) % 5) * 5 + j2];
+
+        else
+        // i1, j2 and i2, j1
+        ciphertext += key[i1 * 5 + j2] + key[i2 * 5 + j1];
+    }
+    return ciphertext;
+}
